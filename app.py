da0c897b0e9973954a0984173bace873a63814fe
ffgtv3 <167542747+ffgtv3@users.noreply.github.com>
2024-07-06 15:54:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
-from flask import Flask, send_file
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Путь к HTML-файлу
-    html_file = 'index.html'
+    return render_template('index.html')
 
-    # Отправить HTML-файл в браузер
-    return send_file(html_file, mimetype='text/html')
+@app.route('/index2')
+def index2():
+    return render_template('index2.html')
 
+@app.route('/index3')
+def index3():
+    return render_template('index3.html')
+
+@app.route('/index4')
+def index4():
+    return render_template('index4.html')
+    
+@app.route('/index5')
+def index5():
+    return render_template('index5.html')
+    
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
